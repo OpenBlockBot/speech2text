4b69e2eb6ca5a073b0502ba68e06e64d5bb5e580
Arthur Zheng <arthurzheng150@gmail.com>
2026-07-22 22:03:14 +0800

    Add Speech to Text extension (ported from Scratch), version 0.1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Speech to Text
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/speech2text?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-audio-lightgrey.svg)
+
+Talk to your projects.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,74 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections should be manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'speech2text.name': 'Speech to Text',
+            'speech2text.description': 'Talk to your projects.'
+        },
+        'ru': {
+            'speech2text.name': 'Речь в текст',
+            'speech2text.description': 'Говорите со своими проектами.'
+        },
+        'zh-cn': {
+            'speech2text.name': '语音转文字',
+            'speech2text.description': '和你的作品对话'
+        },
+        'zh-tw': {
+            'speech2text.name': '語音轉文字',
+            'speech2text.description': '和你的專案對話'
+        }
+    },
+    'extensions': {
+        'en': {
+            'speech.extensionName': 'Speech to Text',
+            'speech.listenAndWait': 'listen and wait',
+            'speech.whenIHear': 'when I hear [PHRASE]',
+            'speech.speechReporter': 'speech',
+            'speech.defaultWhenIHearValue': 'let\'s go'
+        },
+        'ru': {
+            'speech.extensionName': 'Речь в текст',
+            'speech.listenAndWait': 'слушать и ждать',
+            'speech.whenIHear': 'когда я слышу [PHRASE]',
+            'speech.speechReporter': 'речь',
+            'speech.defaultWhenIHearValue': 'поехали'
+        },
+        'zh-cn': {
+            'speech.extensionName': '语音转文字',
+            'speech.listenAndWait': '聆听并等待',
+            'speech.whenIHear': '当我听到 [PHRASE]',
+            'speech.speechReporter': '语音',
+            'speech.defaultWhenIHearValue': '出发'
+        },
+        'zh-tw': {
+            'speech.extensionName': '語音轉文字',
+            'speech.listenAndWait': '聆聽並等待',
+            'speech.whenIHear': '當我聽到 [PHRASE]',
+            'speech.speechReporter': '語音',
+            'speech.defaultWhenIHearValue': '開始'
+        }
+    },
+    'blocks': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    }
+};
